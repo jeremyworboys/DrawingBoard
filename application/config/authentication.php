2371737d50071ef41edbceb2fd9f2b8dc03615f5
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-02-23 18:51:39 +1100

    Created authentication config file

--- application/config/authentication.php
+++ application/config/authentication.php
@@ -15,13 +15,13 @@
 //------------------------------------------------------------------------------
 
 /**
- * Database Config
+ * Authentication Config
  *
- * Defines the database configuration for DrawingBoard.
+ * Defines the configuration for protected controllers.
  *
  * @package		DrawingBoard
  * @subpackage	Config
- * @category 	Database
+ * @category 	Authentication
  * @author		Jeremy Worboys <jeremy@complexcompulsions.com>
  * @since 		Version 0.1
  */
@@ -45,39 +45,3 @@ if (!isset($config)) $config = array();
 // You can edit any values below, but read the comments!
 //------------------------------------------------------------------------------
 
-/**
- * This is the type of database you are running for you application.
- * 
- * Default: mysql
- */
-$config['db_type'] = 'mysql';
-
-/**
- * This is the name of the host that is running your database. It will usually 
- * be localhost.
- * 
- * Default: localhost
- */
-$config['db_host'] = 'localhost';
-
-/**
- * This is the name of the database to connect to.
- */
-$config['db_name'] = 'db_test';
-
-/**
- * This is the username used to connect to the database.
- * 
- * If you are typing "root" here, you are doing it really, really wrong!
- */
-$config['db_user'] = 'root';
-
-
-/**
- * This is the password used to connect to the database.
- */
-$config['db_pass'] = 'root';
-
-/**
- * TODO: sqlite require path to db
- */
