2cd79bf3b7bb09df4635ceea6cb77ab58f67037d
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-02-23 14:00:24 +1100

    Created the protected controller class

---
+++ drawingboard/core/protected_controller.php
@@ -0,0 +1,44 @@
+<?php if (!defined('BASE_PATH')) exit('No direct script access allowed');
+/**
+ * DrawingBoard
+ *
+ * A small lightweight PHP HMVC application framework. Its core design is to be
+ * a simple starting point for small web application projects.
+ *
+ * @package		DrawingBoard
+ * @author		Jeremy Worboys <jeremy@complexcompulsions.com>
+ * @copyright	Copyright (c) 2012 - date('Y', time()), Complex Compulsions
+ * @version     0.1
+ * @filesource
+ */
+
+//------------------------------------------------------------------------------
+
+/**
+ * Protected Controller
+ *
+ * This is a core controller that can be subclassed for areas that can only be 
+ * accessed by authenticated users.
+ *
+ * @package		DrawingBoard
+ * @subpackage	Core
+ * @category 	Controllers
+ * @author		Jeremy Worboys <jeremy@complexcompulsions.com>
+ * @since 		Version 0.1
+ */
+class Protected_controller extends Controller {}
+
+	/**
+	 * Constructor
+	 * 
+	 * Loads in needed configuration and then starts an authentication check.
+	 * 
+	 * @access 		public
+	 * @since 		Version 0.1
+	 * @author		Jeremy Worboys <jeremy@complexcompulsions.com>
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
